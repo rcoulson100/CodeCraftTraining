7656b357175e57ac31f5d2091b1deaa9c0b816f8
Richard Coulson <richard.coulson@tba.group>
2025-03-18 13:14:28 +0000

    refactored move forward tests to simplify test cases

--- CodeCraftMarsRover/CodeCraftMarsRover.Test/MarsRoverMovementTests.cs
+++ CodeCraftMarsRover/CodeCraftMarsRover.Test/MarsRoverMovementTests.cs
@@ -3,53 +3,31 @@ namespace CodeCraftMarsRover.Test;
 [TestFixture]
 public class MarsRoverMovementTests
 {
-    [TestCase(1, 2)]
-    [TestCase(2, 3)]
-    public void MoveForwardWhenFacingNorth(int startPosition, int endPosition)
+    [TestCase(1, 2, Direction.North)]
+    [TestCase(2, 3, Direction.North)]
+    [TestCase(2, 1, Direction.South)]
+    [TestCase(3, 2, Direction.South)]
+    public void MoveForwardWhenFacingNorth(int startPosition, int endPosition, Direction direction)
     {
         Rover rover = new()
         {
             Y = startPosition,
-            Direction = Direction.North
+            Direction = direction
         };
         rover.MoveForward();
         Assert.That(rover.Y, Is.EqualTo(endPosition));
     }
 
-    [TestCase(2, 1)]
-    [TestCase(3, 2)]
-    public void MoveForwardWhenFacingSouth(int startPosition, int endPosition)
-    {
-        Rover rover = new()
-        {
-            Y = startPosition,
-            Direction = Direction.South
-        };
-        rover.MoveForward();
-        Assert.That(rover.Y, Is.EqualTo(endPosition));
-    }
-
-    [TestCase(1,2)]
-    [TestCase(2,3)]
-    public void MoveForwardWhenFacingEast(int startPosition, int endPosition)
+    [TestCase(1, 2, Direction.East)]
+    [TestCase(2, 3, Direction.East)]
+    [TestCase(3, 2, Direction.West)]
+    [TestCase(2, 1, Direction.West)]
+    public void MoveForwardWhenFacingEast(int startPosition, int endPosition, Direction direction)
     {
         Rover rover = new()
         {
             X = startPosition,
-            Direction = Direction.East
-        };
-        rover.MoveForward();
-        Assert.That(rover.X, Is.EqualTo(endPosition));
-    }
-
-    [TestCase(3,2)]
-    [TestCase(2, 1)]
-    public void MoveForwardWhenFacingWest(int startPosition, int endPosition)
-    {
-        Rover rover = new()
-        {
-            X = startPosition,
-            Direction = Direction.West
+            Direction = direction
         };
         rover.MoveForward();
         Assert.That(rover.X, Is.EqualTo(endPosition));
