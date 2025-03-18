ccc14ed36e670666ee0dff5ddbd86de7be061431
Richard Coulson <richard.coulson@tba.group>
2025-03-18 12:02:39 +0000

    Refactor turning tests to reduce test maitenance

--- CodeCraftMarsRover/CodeCraftMarsRover.Test/MarsRoverTurningTests.cs
+++ CodeCraftMarsRover/CodeCraftMarsRover.Test/MarsRoverTurningTests.cs
@@ -3,92 +3,31 @@
 [TestFixture]
 public class MarsRoverTurningTests
 {
-    [Test]
-    public void TurnRightWhenFacingNorth()
+    [TestCase(Direction.North, Direction.East)]
+    [TestCase(Direction.East, Direction.South)]
+    [TestCase(Direction.South, Direction.West)]
+    [TestCase(Direction.West, Direction.North)]
+    public void TurnRight(Direction originalDirection, Direction expectedDirection)
     {
         Rover rover = new()
         {
-            Direction = Direction.North
+            Direction = originalDirection
         };
         rover.TurnRight();
-        Assert.That(rover.Direction, Is.EqualTo(Direction.East));
+        Assert.That(rover.Direction, Is.EqualTo(expectedDirection));
     }
 
-    [Test]
-    public void TurnRightWhenFacingEast()
+    [TestCase(Direction.North, Direction.West)]
+    [TestCase(Direction.West, Direction.South)]
+    [TestCase(Direction.South, Direction.East)]
+    [TestCase(Direction.East, Direction.North)]
+    public void TurnLeft(Direction originalDirection, Direction expectedDirection)
     {
         Rover rover = new()
         {
-            Direction = Direction.East
-        };
-        rover.TurnRight();
-        Assert.That(rover.Direction, Is.EqualTo(Direction.South));
-    }
-
-    [Test]
-    public void TurnRightWhenFacingSouth()
-    {
-        Rover rover = new()
-        {
-            Direction = Direction.South
-        };
-        rover.TurnRight();
-        Assert.That(rover.Direction, Is.EqualTo(Direction.West));
-    }
-
-    [Test]
-    public void TurnRightWhenFacingWest()
-    {
-        Rover rover = new()
-        {
-            Direction = Direction.West
-        };
-        rover.TurnRight();
-        Assert.That(rover.Direction, Is.EqualTo(Direction.North));
-    }
-
-    [Test]
-    public void TurnLeftWhenFacingNorth()
-    {
-        Rover rover = new()
-        {
-            Direction = Direction.North
+            Direction = originalDirection
         };
         rover.TurnLeft();
-        Assert.That(rover.Direction, Is.EqualTo(Direction.West));
+        Assert.That(rover.Direction, Is.EqualTo(expectedDirection));
     }
-
-    [Test]
-    public void TurnLeftWhenFacingWest()
-    {
-        Rover rover = new()
-        {
-            Direction = Direction.West
-        };
-        rover.TurnLeft();
-        Assert.That(rover.Direction, Is.EqualTo(Direction.South));
-    }
-
-    [Test]
-    public void TurnLeftWhenFacingSouth()
-    {
-        Rover rover = new()
-        {
-            Direction = Direction.South
-        };
-        rover.TurnLeft();
-        Assert.That(rover.Direction, Is.EqualTo(Direction.East));
-    }
-
-    [Test]
-    public void TurnLeftWhenFacingEast()
-    {
-        Rover rover = new()
-        {
-            Direction = Direction.East
-        };
-        rover.TurnLeft();
-        Assert.That(rover.Direction, Is.EqualTo(Direction.North));
-    }
-
 }
