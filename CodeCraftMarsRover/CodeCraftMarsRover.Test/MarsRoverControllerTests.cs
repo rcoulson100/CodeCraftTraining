c06f55006b71c3a3fd9b57b4dedab38e530cde77
Richard Coulson <richard.coulson@tba.group>
2025-03-18 13:32:08 +0000

    Added a controller

---
+++ CodeCraftMarsRover/CodeCraftMarsRover.Test/MarsRoverControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCraftMarsRover.Test;
+
+[TestFixture]
+public class MarsRoverControllerTests
+{
+    [Test]
+    public void DoNothingWhenEmptyCommand()
+    {
+        Rover rover = new()
+        {
+            X = 1,
+            Y = 1,
+            Direction = Direction.North
+        };
+
+        RoverController controller = new()
+        {
+            Rover = rover
+        };
+
+        controller.ExecuteCommand("");
+
+        Assert.That(rover.X, Is.EqualTo(1));
+        Assert.That(rover.Y, Is.EqualTo(1));
+        Assert.That(rover.Direction, Is.EqualTo(Direction.North));
+    }
+}
---
+++ CodeCraftMarsRover/CodeCraftMarsRover.Test/RoverController.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace CodeCraftMarsRover.Test;
+
+public class RoverController
+{
+    public Rover Rover { get; set; } = default!;
+
+    public void ExecuteCommand(string instruction)
+    {
+
+    }
+}
