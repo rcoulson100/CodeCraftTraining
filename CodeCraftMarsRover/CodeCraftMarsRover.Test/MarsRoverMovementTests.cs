032fbf0b6a0952319d2141ccebc851e5e42e2c21
Richard Coulson <richard.coulson@tba.group>
2025-03-18 13:17:45 +0000

    refactored move backwards tests to simplify test cases

--- CodeCraftMarsRover/CodeCraftMarsRover.Test/MarsRoverMovementTests.cs
+++ CodeCraftMarsRover/CodeCraftMarsRover.Test/MarsRoverMovementTests.cs
@@ -33,54 +33,32 @@ public class MarsRoverMovementTests
         Assert.That(rover.X, Is.EqualTo(endPosition));
     }
 
-    [TestCase(2, 1)]
-    [TestCase(3, 2)]
-    public void MoveBackwardWhenFacingNorth(int startPosition, int endPosition)
+    [TestCase(2, 1, Direction.North)]
+    [TestCase(3, 2, Direction.North)]
+    [TestCase(1, 2, Direction.South)]
+    [TestCase(2, 3, Direction.South)]
+    public void MoveBackwardWhenFacingNorth(int startPosition, int endPosition, Direction direction)
     {
         Rover rover = new()
         {
             Y = startPosition,
-            Direction = Direction.North
+            Direction = direction
         };
         rover.MoveBackward();
 
         Assert.That(rover.Y, Is.EqualTo(endPosition));
     }
 
-    [TestCase(2, 1)]
-    [TestCase(3, 2)]
-    public void MoveBackwardWhenFacingEast(int startPosition, int endPosition)
+    [TestCase(2, 1, Direction.East)]
+    [TestCase(3, 2, Direction.East)]
+    [TestCase(1, 2, Direction.West)]
+    [TestCase(2, 3, Direction.West)]
+    public void MoveBackwardWhenFacingEast(int startPosition, int endPosition, Direction direction)
     {
         Rover rover = new()
         {
             X = startPosition,
-            Direction = Direction.East
-        };
-        rover.MoveBackward();
-        Assert.That(rover.X, Is.EqualTo(endPosition));
-    }
-
-    [TestCase(1, 2)]
-    [TestCase(2, 3)]
-    public void MoveBackwardWhenFacingSouth(int startPosition, int endPosition)
-    {
-        Rover rover = new()
-        {
-            Y = startPosition,
-            Direction = Direction.South
-        };
-        rover.MoveBackward();
-        Assert.That(rover.Y, Is.EqualTo(endPosition));
-    }
-
-    [TestCase(1, 2)]
-    [TestCase(2, 3)]
-    public void MoveBackwardWhenFacingWest(int startPosition, int endPosition)
-    {
-        Rover rover = new()
-        {
-            X = startPosition,
-            Direction = Direction.West
+            Direction = direction
         };
         rover.MoveBackward();
         Assert.That(rover.X, Is.EqualTo(endPosition));
